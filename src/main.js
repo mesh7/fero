e885ada1f48333fd4df49e2c560efdfd9567fff6
Meshram <shubham.meshram@capgemini.com>
2025-08-09 18:13:48 +0530

    added basic folder structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: ".dark",
+    },
+  },
+});
+app.mount("#app");
---
+++ src/views/Landing-page/LandingPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <p>
+    Landing page
+    </p>
+</template>
+
+<style scoped>
+</style>
